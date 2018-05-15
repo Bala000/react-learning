f2fefd2fac88dfb7574828549cfe890c38b7cf2a
Balachandar <balachandarsam2011@gmail.com>
2018-05-15 10:02:28 -0700

    so far

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
-
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -51,13 +51,14 @@ class App extends Component {
        {
          this.state.persons.map( (person,index) => {
            return (
+             <ErrorBoundary key={person.id}>
            <Person 
             name ={person.name} 
             age={person.age}
             click={() => this.deletePersonHandler(index)}
-            key={person.id}
+            
             change={(event) => this.nameChangeHandler(event, person.id)}
-            />
+            /> </ErrorBoundary>
           );
          })
        }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import classes from './Person.css';
 const person = (props) => {
+    const rand = Math.random();
+    if(rand>0.7)
+    {
+        throw new Error("artificial error thrown");
+    }
     return (
     <div className={classes.Person}>
         <p onClick = {props.click}>I am {props.name} and I am {props.age} years young</p>
